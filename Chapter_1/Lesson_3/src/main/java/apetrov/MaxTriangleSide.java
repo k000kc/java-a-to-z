9b846e50dc5d24be729a9f5e2cc6e21fb5a8a436
k000kc <ham2188@mail.ru>
2016-09-11 18:07:28 +0300

    update MaxTriangleSide

--- Chapter_1/Lesson_3/src/main/java/apetrov/MaxTriangleSide.java
+++ Chapter_1/Lesson_3/src/main/java/apetrov/MaxTriangleSide.java
@@ -1,7 +1,15 @@
 package apetrov;
 
+/**
+* Класс для определения самой длинной стороны треугольника
+* @author Andrey
+*/
 public class MaxTriangleSide {
-   
+
+    /**
+    * Определяем самую длинную сторону треугольника
+    * @param args 
+    */   
     public double maxSide(double ab, double bc, double ca){
         return Math.max(ab, Math.max(bc, ca));
     } 

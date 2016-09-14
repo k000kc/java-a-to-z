4847ed3b3b98e7a832a52bca20544de909f313ce
k000kc <ham2188@mail.ru>
2016-09-14 22:06:40 +0300

    update

--- Chapter_1/Lesson_5/src/test/java/apetrov/RemoveDuplicateArrayTest.java
+++ Chapter_1/Lesson_5/src/test/java/apetrov/RemoveDuplicateArrayTest.java
@@ -11,7 +11,7 @@ public class RemoveDuplicateArrayTest{
 		String[] inArray = new String[]{"2","1","6","1","3","7","2","1","5","3"};
 		String[] outArray = new String[]{"2","1","6","3","7","5"};
 		RemoveDuplicateArray array = new RemoveDuplicateArray(inArray);
-		String[] result = array.rmDuplicate(inArray);
-		assertThat(result, is(outArray));
+		inArray = array.rmDuplicate(inArray);
+		assertThat(inArray, is(outArray));
 	}
 }

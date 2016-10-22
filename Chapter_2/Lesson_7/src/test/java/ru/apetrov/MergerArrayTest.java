a3d7ba2feba9e50e9e9526a0ee4be761e7c9ea52
k000kc <ham2188@mail.ru>
2016-10-22 16:16:38 +0300

    add ChessUML

--- Chapter_2/Lesson_7/src/test/java/ru/apetrov/MergerArrayTest.java
+++ Chapter_2/Lesson_7/src/test/java/ru/apetrov/MergerArrayTest.java
@@ -5,12 +5,12 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class MergerArrayTest{
-	int[] a = new int[]{1, 3, 6, 7, 9, 12};
-	int[] b = new int[]{2, 4, 5, 8, 10, 11, 14};
-	int[] result = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14};
 
 	@Test
 	public void whenInputTwoArratThenOutputResultArray(){
+		int[] a = new int[]{1, 3, 6, 7, 9, 12};
+		int[] b = new int[]{2, 4, 5, 8, 10, 11, 14};
+		int[] result = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14};
 		MergerArray mergerArray = new MergerArray();
 		int[] array = mergerArray.merger(a, b);
 		assertThat(result, is(array));

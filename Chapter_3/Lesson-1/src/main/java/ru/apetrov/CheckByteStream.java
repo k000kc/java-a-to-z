9ba7b73246464a694f0ef30045632bcdc6266d85
k000kc <ham2188@mail.ru>
2016-11-05 19:42:21 +0300

    add CheckByteStream

---
+++ Chapter_3/Lesson-1/src/main/java/ru/apetrov/CheckByteStream.java
@@ -0,0 +1,19 @@
+package ru.apetrov;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by Andrey on 05.11.2016.
+ */
+public class CheckByteStream{
+
+    public boolean isNumber(InputStream in) throws IOException{
+        boolean result = false;
+
+        if (in.read() % 2 == 0){
+            result = true;
+        }
+        return result;
+    }
+}
---
+++ Chapter_3/Lesson-1/src/test/java/ru/apetrov/CheckByteStreamTest.java
@@ -0,0 +1,16 @@
+package ru.apetrov;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Created by Andrey on 05.11.2016.
+ */
+@Ignore
+public class CheckByteStreamTest {
+
+    @Test
+    public void whenInputStreamThenCheckNumber(){
+
+    }
+}

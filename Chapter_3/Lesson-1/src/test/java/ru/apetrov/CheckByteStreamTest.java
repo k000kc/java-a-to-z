ef4bd475a82b8584d8490500e55db2e027ed9564
k000kc <ham2188@mail.ru>
2016-11-06 12:33:47 +0300

    add CheckByteStreamTest

--- Chapter_3/Lesson-1/src/test/java/ru/apetrov/CheckByteStreamTest.java
+++ Chapter_3/Lesson-1/src/test/java/ru/apetrov/CheckByteStreamTest.java
@@ -1,16 +1,41 @@
 package ru.apetrov;
 
+import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
+
+import java.io.*;
 
 /**
  * Created by Andrey on 05.11.2016.
  */
-@Ignore
+//@Ignore
 public class CheckByteStreamTest {
 
-    @Test
-    public void whenInputStreamThenCheckNumber(){
+    CheckByteStream cbs = new CheckByteStream();
+    boolean result = false;
 
+    @Test
+    public void whenInputEvenNumberThenCheckNumber(){
+        InputStream inputStream = new ByteArrayInputStream("4".getBytes());
+        try {
+            result = cbs.isNumber(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assertTrue(result);
+    }
+
+    @Test
+    public void whenInputOddNumberThenCheckNumber(){
+        InputStream inputStream = new ByteArrayInputStream("3".getBytes());
+        try {
+            result = cbs.isNumber(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assertFalse(result);
     }
 }

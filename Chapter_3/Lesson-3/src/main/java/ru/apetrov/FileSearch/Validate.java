1609cfe9f335af0ee1eb86d70dca37a7e5e45680
k000kc <ham2188@mail.ru>
2017-01-07 14:16:12 +0300

    update FileSearch

--- Chapter_3/Lesson-3/src/main/java/ru/apetrov/FileSearch/Validate.java
+++ Chapter_3/Lesson-3/src/main/java/ru/apetrov/FileSearch/Validate.java
@@ -28,24 +28,23 @@ public class Validate {
 
     /**
      * Проверка введенной комманды.
-     * @param args массив ключей
      * @return результат
      */
-    private boolean checkArgs(String[] args) {
+    private boolean checkArgs() {
         boolean result = true;
         if (!this.args[0].equals("-d")) {
             result = false;
         }
 
-        if (!args[2].equals("-n")) {
+        if (!this.args[2].equals("-n")) {
             result = false;
         }
 
-        if (!args[4].equals("-m") && !args[4].equals("-f") && !args[4].equals("-r")) {
+        if (!this.args[4].equals("-m") && !this.args[4].equals("-f") && !this.args[4].equals("-r")) {
             result = false;
         }
 
-        if (!args[5].equals("-o")) {
+        if (!this.args[5].equals("-o")) {
             result = false;
         }
         return result;
@@ -55,18 +54,18 @@ public class Validate {
      * Выбор типа поиска.
      */
     public void isValidate() {
-        if (this.checkArgs(this.args) && this.dir.isDirectory()) {
+        if (this.checkArgs() && this.dir.isDirectory()) {
 
             if (this.args[4].equals("-m")) {
-                System.out.println(new FindByMask(this.dir, args[3]));
+                System.out.println(new FindByMask(this.dir, this.args[3]));
             }
 
-            if (args[4].equals("-f")) {
-                System.out.println(new FindByName(this.dir, args[3]));
+            if (this.args[4].equals("-f")) {
+                System.out.println(new FindByName(this.dir, this.args[3]));
             }
 
-            if (args[4].equals("-r")) {
-                System.out.println(new FindByRegExp(this.dir, args[3]));
+            if (this.args[4].equals("-r")) {
+                System.out.println(new FindByRegExp(this.dir, this.args[3]));
             }
         } else {
             System.out.printf("%s", "Ключи \n-d - директория в которая начинать поиск.\n-n - имя файл, маска, либо регулярное выражение.\n-m - искать по маске, либо -f - полное совпадение имени. -r - регулярное выражение.\n-o - результат записать в файл.");

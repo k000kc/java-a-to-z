6550e02963091cda85b25c2304894a458a39f7ba
k000kc <ham2188@mail.ru>
2017-02-14 18:29:49 +0300

    update SimpleGenerator

--- Chapter_4/Lesson-4.6.1/src/main/java/ru/apetrov/KeyNotFoundExeption.java
+++ Chapter_4/Lesson-4.6.1/src/main/java/ru/apetrov/KeyNotFoundExeption.java
@@ -1,11 +1,10 @@
 package ru.apetrov;
 
-import java.util.IllegalFormatException;
 
 /**
  * Created by Andrey on 14.02.2017.
  */
-public class KeyNotFoundExeption extends Exception {
+public class KeyNotFoundExeption extends IllegalStateException {
     public KeyNotFoundExeption(String message) {
         super(message);
     }

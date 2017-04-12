26808504aa8973b0b7cee3b768fcfe9d480bc230
k000kc <ham2188@mail.ru>
2017-04-12 17:09:17 +0300

    add lesson 5.5.3 - Main

--- Chapter_5/Lesson-5.5.1/src/main/java/ru/apetrov/user/User.java
+++ Chapter_5/Lesson-5.5.1/src/main/java/ru/apetrov/user/User.java
@@ -57,4 +57,16 @@ public class User {
     public Calendar getBirthday() {
         return birthday;
     }
+
+    /**
+     * hash code.
+     * @return hash code
+     */
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + children;
+        result = 31 * result + (birthday != null ? birthday.hashCode() : 0);
+        return result;
+    }
 }

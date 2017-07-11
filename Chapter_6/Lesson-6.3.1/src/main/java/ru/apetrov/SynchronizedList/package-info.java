99ec0c6f2f69ffc0c5d2046b70e4c3a6d34f43f4
k000kc <ham2188@mail.ru>
2017-07-11 20:17:09 +0300

    addLesson-6.3.3

---
+++ Chapter_6/Lesson-6.3.1/src/main/java/ru/apetrov/SynchronizedList/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Andrey on 11.07.2017.
+ *
+ * Реализовать ThreadSafe динамический список и связанный список из заданий модуля коллекции.
+ */
+package ru.apetrov.SynchronizedList;

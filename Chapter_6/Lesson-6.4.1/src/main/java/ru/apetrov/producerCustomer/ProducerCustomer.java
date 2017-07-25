445021fc1ba58ba77a15de658c362ebac9432ab7
k000kc <ham2188@mail.ru>
2017-07-25 20:42:26 +0300

    # This is a combination of 5 commits. # The first commit's message is: add 6.4.1
    # This is the 2nd commit message:
    
    add 6.4.1
    
    # This is the 3rd commit message:
    
    update 6.4.1
    
    # This is the 4th commit message:
    
    update 6.4.1
    
    # This is the 5th commit message:
    
    update 6.4.1

---
+++ Chapter_6/Lesson-6.4.1/src/main/java/ru/apetrov/producerCustomer/ProducerCustomer.java
@@ -0,0 +1,79 @@
+package ru.apetrov.producerCustomer;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Created by Andrey on 26.07.2017.
+ */
+public class ProducerCustomer<E> {
+
+    private Queue<E> queue;
+
+    private boolean isLock = false;
+
+    public ProducerCustomer() {
+        this.queue = new LinkedList<E>();
+    }
+
+    public void add(E element) {
+        synchronized (this.queue) {
+            while (this.isLock) {
+                try {
+                    this.queue.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        this.queue.add(element);
+        System.out.printf("add element %s\n", element);
+
+        synchronized (this.queue) {
+            this.queue.notify();
+            this.isLock = true;
+        }
+    }
+
+    public void get() {
+        synchronized (this.queue) {
+            while (!this.isLock || this.queue.peek() == null) {
+                try {
+                    this.queue.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        System.out.printf("get %s\n", this.queue.poll());
+
+        synchronized (this.queue) {
+            this.queue.notify();
+            this.isLock = false;
+        }
+    }
+
+    public static void main(String[] args) {
+        ProducerCustomer<Integer> prodCust = new ProducerCustomer<>();
+        Thread producer = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    prodCust.add(i);
+                }
+            }
+        });
+
+        Thread customer = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    prodCust.get();
+                }
+            }
+        });
+
+        producer.start();
+        customer.start();
+    }
+}

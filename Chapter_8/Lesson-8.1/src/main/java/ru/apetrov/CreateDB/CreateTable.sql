81a746e63e90bd364b6d075125e0f1a4e5185c0f
k000kc <ham2188@mail.ru>
2017-10-24 20:19:53 +0300

    add sql script

---
+++ Chapter_8/Lesson-8.1/src/main/java/ru/apetrov/CreateDB/CreateTable.sql
@@ -0,0 +1,57 @@
+﻿create table user_storage (
+	id serial primary key,
+	nickname character varying(20)
+);
+
+create table user_info (
+	id serial primary key,
+	fullname character varying(200),
+	phone character(16),
+	storage_id integer references user_storage(id)
+);
+
+create table roles (
+	id serial primary key,
+	type_role character varying(20)
+);
+
+create table user_role (
+	user_id integer references user_info(id),
+	role_id integer references roles(id)
+);
+
+create table items_storage (
+	id serial primary key,
+	user_id integer references user_info(id),
+	name_item character varying(200)
+);
+
+create table item_info (
+	id serial primary key,
+	status character varying(20),
+	category character varying(20),
+	storage_id integer references items_storage(id)
+);
+
+create table comments_storage (
+	id serial primary key,
+	symmary character varying(200),
+	item_id integer references item_info(id)
+);
+
+create table comment (
+	id serial primary key,
+	description text,
+	storage_id integer references comments_storage(id)
+);
+
+create table file_storage (
+	id serial primary key,
+	comment_id integer references comment(id)
+);
+
+create table file_info (
+	id serial primary key,
+	filename character varying(50),
+	starage_id integer references file_storage(id)
+);
---
+++ Chapter_8/Lesson-8.1/src/main/java/ru/apetrov/CreateDB/InsertIntoTable.sql
@@ -0,0 +1,20 @@
+﻿insert into user_storage(nickname) values ('apetrov'), ('user');
+
+insert into user_info (fullname, phone, storage_id) values('Petrov Andrey Anatolevich', '+7(123)456-78-90', 1);
+insert into user_info (fullname, phone, storage_id) values('Testov Test Testovich', '+7(123)908-76-54', 2);
+
+insert into roles(type_role) values('user'), ('admin');
+
+insert into user_role(user_id, role_id) values(1, 1), (2, 2);
+
+insert into items_storage(user_id, name_item) values(1, 'Error 404'), (2, 'Ничего не работает');
+
+insert into item_info(status, category, storage_id) values('На выполнении', 'СРЕДНИЙ', 1), ('ВЫПОЛНЕН','НЕ СРОЧНЫЙ', 2);
+
+insert into comments_storage(symmary, item_id) values('bla bla bla', 1), ('pam pam', 2); 
+
+insert into comment(description, storage_id) values('this is description', 1), ('description too', 2);
+
+insert into file_storage(comment_id) values(1), (2);
+
+insert into file_info(filename, starage_id) values ('d:\dfdfdfd.txt', 1), ('d:\dfgdf.jpg', 2);

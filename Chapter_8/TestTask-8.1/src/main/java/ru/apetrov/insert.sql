6bb0c88a3e7d8e2976dc3943ac77d42857d041f9
k000kc <ham2188@mail.ru>
2017-12-28 18:06:58 +0300

    add querys

---
+++ Chapter_8/TestTask-8.1/src/main/java/ru/apetrov/insert.sql
@@ -0,0 +1,32 @@
+﻿insert into company(id, name) values(1, 'first company'); 
+insert into company(id, name) values(2, 'second company'); 
+insert into company(id, name) values(3, 'third company'); 
+insert into company(id, name) values(4, 'fourth company'); 
+insert into company(id, name) values(5, 'fifth company'); 
+insert into company(id, name) values(6, 'sixth company'); 
+insert into company(id, name) values(7, 'seventh company'); 
+insert into company(id, name) values(8, 'eighth company'); 
+insert into company(id, name) values(9, 'ninth company'); 
+
+
+insert into person(id, name, company_id) values(1, '1 employee', 1); 
+insert into person(id, name, company_id) values(2, '2 employee', 1); 
+insert into person(id, name, company_id) values(3, '3 employee', 1); 
+insert into person(id, name, company_id) values(4, '4 employee', 2); 
+insert into person(id, name, company_id) values(5, '5 employee', 3); 
+insert into person(id, name, company_id) values(6, '6 employee', 3); 
+insert into person(id, name, company_id) values(7, '7 employee', 3); 
+insert into person(id, name, company_id) values(8, '8 employee', 3); 
+insert into person(id, name, company_id) values(9, '9 employee', 4); 
+insert into person(id, name, company_id) values(10, '10 employee', 4); 
+insert into person(id, name, company_id) values(11, '11 employee', 5); 
+insert into person(id, name, company_id) values(12, '12 employee', 5); 
+insert into person(id, name, company_id) values(13, '13 employee', 6); 
+insert into person(id, name, company_id) values(14, '14 employee', 7); 
+insert into person(id, name, company_id) values(15, '15 employee', 7); 
+insert into person(id, name, company_id) values(16, '16 employee', 8); 
+insert into person(id, name, company_id) values(17, '17 employee', 9); 
+insert into person(id, name, company_id) values(18, '18 employee', 9); 
+insert into person(id, name, company_id) values(19, '19 employee', 9); 
+insert into person(id, name, company_id) values(20, '20 employee', 9); 
+
---
+++ Chapter_8/TestTask-8.1/src/main/java/ru/apetrov/query2.sql
@@ -0,0 +1,7 @@
+﻿select c.name, p.company_id from company as c
+inner join person as p on c.id = p.company_id and p.company_id 
+in (select max(p.company_id) from company as c 
+inner join person as p on c.id = p.company_id and p.company_id 
+in (select count(p.company_id) from company as c
+inner join person as p
+on c.id = p.company_id group by c.name));

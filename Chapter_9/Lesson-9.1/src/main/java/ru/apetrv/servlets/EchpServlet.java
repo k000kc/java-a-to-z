2aa888145bde0b6c9893361f4b1c1000801919c3
k000kc <ham2188@mail.ru>
2018-01-29 21:23:23 +0300

    add Lesson-9.1 - servlets

---
+++ Chapter_9/Lesson-9.1/src/main/java/ru/apetrv/servlets/EchpServlet.java
@@ -0,0 +1,25 @@
+package ru.apetrv.servlets;
+
+import org.apache.log4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Andrey on 29.01.2018.
+ */
+public class EchpServlet extends HttpServlet {
+    private static final Logger log = Logger.getLogger(EchpServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = new PrintWriter(resp.getOutputStream());
+        writer.append("hello world");
+        writer.flush();
+    }
+}

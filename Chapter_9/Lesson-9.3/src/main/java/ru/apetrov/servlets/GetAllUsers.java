59e5213d65814ecb38b222add886edcd1dac5327
k000kc <ham2188@mail.ru>
2018-02-15 21:21:25 +0300

    add Lesson-9.3

--- Chapter_9/Lesson-9.3/src/main/java/ru/apetrov/servlets/GetAllUsers.java
+++ Chapter_9/Lesson-9.3/src/main/java/ru/apetrov/servlets/GetAllUsers.java
@@ -2,6 +2,7 @@ package ru.apetrov.servlets;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ru.apetrov.model.User;
 import ru.apetrov.storege.UserStore;
 
 import javax.servlet.ServletException;
@@ -23,7 +24,22 @@ public class GetAllUsers extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
-        PrintWriter writer = resp.getWriter();
-        writer.append("HI");
+        PrintWriter writer = new PrintWriter(resp.getOutputStream());
+        writer.append("<!DOCTYPE html>" +
+                "<html lang=\"en\">" +
+                "<head>" +
+                "    <meta charset=\"UTF-8\">" +
+                "    <title>Title</title>" +
+                "</head>" +
+                "<body>" +
+                "<form action='" + req.getContextPath() + "/allusers' method='post'>" +
+                "Name: <input type='text' name='login'/>" +
+                "<input type='submit'/>" +
+                "</form>" +
+                "<br/>" +
+                this.userStore.getAll().toString() +
+                "</body>" +
+                "</html>");
+        writer.flush();
     }
 }

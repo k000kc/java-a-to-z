68a4a62844b1e4f8266f305e02bfc09632859692
k000kc <ham2188@mail.ru>
2018-03-02 20:09:35 +0300

    lesson-9.5 add MVC

--- Chapter_9/Lesson-9.5/src/main/java/ru/apetrov/controller/ShowUser.java
+++ Chapter_9/Lesson-9.5/src/main/java/ru/apetrov/controller/ShowUser.java
@@ -1,5 +1,7 @@
 package ru.apetrov.controller;
 
+import ru.apetrov.model.UserStore;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,9 +10,12 @@ import java.io.IOException;
 
 public class ShowUser extends HttpServlet {
 
+    private final UserStore store = UserStore.getInstance();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        req.setAttribute("users", store.getAll());
+        req.getRequestDispatcher("/WEB-INF/views/ShowUsers.jsp").forward(req, resp);
     }
 
     @Override

7ffd904f2684e70226d47cf19a31e7205a8ba8a3
k000kc <ham2188@mail.ru>
2018-05-24 21:55:00 +0300

    TestTask-9: (реализация метода create у RpleImpl)

--- Chapter_9/TestTask-9/src/main/java/ru/apetrov/dao/RoleImpl.java
+++ Chapter_9/TestTask-9/src/main/java/ru/apetrov/dao/RoleImpl.java
@@ -2,13 +2,23 @@ package ru.apetrov.dao;
 
 import ru.apetrov.models.Role;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Set;
 
 public class RoleImpl extends ModelBaseDAO<Role,Long> {
 
     @Override
     public void create(Role role) {
-
+        Connection connection = super.getConnection();
+        try (PreparedStatement statement = connection.prepareStatement("INSERT INTO roles(id, role) VALUES(?, ?)")) {
+            statement.setInt(1, role.getId());
+            statement.setString(2, role.getRoleType());
+            statement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override

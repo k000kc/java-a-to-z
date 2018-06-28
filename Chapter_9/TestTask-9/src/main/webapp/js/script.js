6c227646e40ac73f9114da59ae7fff9bdfe17641
k000kc <ham2188@mail.ru>
2018-06-28 22:21:51 +0300

    TestTask-9 (update ajax script for the show users servlet)

--- Chapter_9/TestTask-9/src/main/webapp/js/script.js
+++ Chapter_9/TestTask-9/src/main/webapp/js/script.js
@@ -10,15 +10,41 @@
 //         })
 //     });
 // })
-$(document).ready(function () {
+$(document).ready(function getall () {
         $.getJSON("show", function (data) {
-            var result = "<table>";
+            var result = "";
             $.each(data, function (index, users) {
-                console.log(users.login);
-                result += "<tr><td>" + users.login + "</tr><td>";
-
+                result += "<tr><td>" + users.login + "</td>";
+                result += "<td>" + users.name + "</td>";
+                result += "<td>" + users.email + "</td>";
+                result += "<td>" + users.address.country + "</td>";
+                result += "<td>" + users.address.city + "</td>";
+                result += "<td>" + users.address.street + "</td>";
+                result += "<td>" + users.address.house + "</td>";
+                result += "<td>" + users.role.roleType + "</td>";
+                result += "<td>";
+                for (var i = 0; i < users.musicTypes.length; i++) {
+                    result += users.musicTypes[i].musicType + ", ";
+                }
+                result += "</td>";
+                result += returnbutton(users.login);
+                result += "</tr>";
             });
-            var usertable = $("#allusers");
+            var usertable = $("#userstable");
             $(result).appendTo(usertable);
         });
 });
+
+function returnbutton(login) {
+    var buttons = "<td><form action='/update' method='get'>" +
+                    "<input type='button' value='update' class='button'>" +
+                    "<input type='hidden' name='login' value='" + login + "'>" +
+                "</form>" +
+                "</td>" +
+                "<td>" +
+                "<form action='/delete' method='get'>" +
+                    "<input type='button' value='delete' class='button'>" +
+                    "<input type='hidden' name='login' value='" + login + "'>" +
+                "</form></td>";
+    return buttons;
+}

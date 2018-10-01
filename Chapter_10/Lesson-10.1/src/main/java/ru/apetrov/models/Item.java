23f40090f8b63d4f6fd86ff653384ce6c149ae6c
k000kc <ham2188@mail.ru>
2018-10-01 21:21:02 +0300

    add lesson-10.1

---
+++ Chapter_10/Lesson-10.1/src/main/java/ru/apetrov/models/Item.java
@@ -0,0 +1,60 @@
+package ru.apetrov.models;
+
+import java.sql.Timestamp;
+import java.util.Objects;
+
+public class Item {
+
+    private int id;
+    private String desc;
+    private Timestamp created;
+    private boolean done;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public Timestamp getCreated() {
+        return created;
+    }
+
+    public void setCreated(Timestamp created) {
+        this.created = created;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return id == item.id &&
+                done == item.done &&
+                Objects.equals(desc, item.desc) &&
+                Objects.equals(created, item.created);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, desc, created, done);
+    }
+}

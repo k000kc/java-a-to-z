1b7ed373dcccfc2df043c1dbf6041a50f14a1d13
k000kc <ham2188@mail.ru>
2018-10-04 20:54:46 +0300

    add lesson-10.1 (servlet)

---
+++ Chapter_10/Lesson-10.1/src/main/java/ru/apetrov/controller/ShowPage.java
@@ -0,0 +1,15 @@
+package ru.apetrov.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ShowPage extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher(String.format("%s/view/index.html", req.getContextPath())).forward(req, resp);
+    }
+}

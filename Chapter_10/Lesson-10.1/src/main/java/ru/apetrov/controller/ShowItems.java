fca407c0958b6a0c913bc880015457bbae879c7d
k000kc <ham2188@mail.ru>
2018-10-12 19:31:40 +0300

    lesson-10.1 (add form: show items)

---
+++ Chapter_10/Lesson-10.1/src/main/java/ru/apetrov/controller/ShowItems.java
@@ -0,0 +1,42 @@
+package ru.apetrov.controller;
+
+import com.google.gson.Gson;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import ru.apetrov.models.Item;
+import ru.apetrov.repository.ItemStore;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+public class ShowItems extends HttpServlet {
+
+    private SessionFactory factory;
+    private ItemStore store;
+
+    @Override
+    public void init() throws ServletException {
+        this.factory = new Configuration().configure().buildSessionFactory();
+        this.store = new ItemStore(factory);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+        PrintWriter writer = resp.getWriter();
+
+        List<Item> items = store.getAll();
+
+        String gson = new Gson().toJson(items);
+        writer.write(gson);
+
+        factory.close();
+        writer.flush();
+        writer.close();
+    }
+}
--- Chapter_10/Lesson-10.1/src/main/webapp/js/scripts.js
+++ Chapter_10/Lesson-10.1/src/main/webapp/js/scripts.js
@@ -1,9 +1,8 @@
 $(document).ready(function () {
-    console.log("click");
     $("#button_item").click(function () {
-        console.log("click2");
         create_item();
-    })
+    });
+    getItems();
 });
 
 function create_item() {
@@ -17,4 +16,34 @@ function create_item() {
             console.log("created");
         }
     });
+}
+
+function getItems() {
+    console.log("ggg");
+    $.getJSON("get_items", function (data) {
+        var result = "<tr>" +
+            "<th>id</th>" +
+            "<th>description</th>" +
+            "<th>created</th>" +
+            "<th>done</th>" +
+            "</tr>";
+        $.each(data, function (index, items) {
+            result += "<tr><td>" + items.id + "</td>" +
+                "<td>" + items.desc + "</td>" +
+                "<td>" + items.created + "</td>" +
+                "<td>" + get_checkbox(items) + "</td></tr>";
+        });
+        var table = $("#items_table");
+        $(result).appendTo(table);
+    })
+}
+
+function get_checkbox(items) {
+    var checkbox = "<td>" +
+        "<input type='checkbox' ";
+    if (items.done == true)
+        checkbox += "checked";
+    checkbox += ">" +
+        "<input type='hidden' value='" + items.id + "'>";
+    return checkbox;
 }
